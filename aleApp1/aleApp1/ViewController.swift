2c10564bc7d04153799b4a04ec00cf0578bb3840
Gustavo Barbeito <gustavo.barbeito@hotmail.com>
2017-03-13 22:27:06 -0300

    se creo funcionalidad boton3 y se aplico un actionsheet con 3 opciones y cancelar

--- aleApp1/aleApp1/ViewController.swift
+++ aleApp1/aleApp1/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
     @IBAction func Click(_ sender: UIButton) {
         
         let controladorAlerta = UIAlertController(title: "Alerta", message: "Esto es Una Alerta", preferredStyle: .alert)
@@ -26,6 +28,30 @@ class ViewController: UIViewController {
         
     }
 
+    
+    @IBAction func actionSheet(_ sender: UIButton) {
+    
+    
+        let actionSheetController = UIAlertController(title: "Action Sheet", message: "Elija Una                                                                Opcion", preferredStyle: .actionSheet)
+        
+        let opcion1 = UIAlertAction(title: "Opcion 1", style: .default, handler: nil)
+        
+        let opcion2 = UIAlertAction(title: "Opcion 2", style: .default, handler: nil)
+        
+        let opcion3 = UIAlertAction(title: "Opcion 3", style: .default, handler: nil)
+        
+        let cancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        actionSheetController.addAction(opcion1)
+        actionSheetController.addAction(opcion2)
+        actionSheetController.addAction(opcion3)
+        actionSheetController.addAction(cancelar)
+        
+        present(actionSheetController, animated: true, completion: nil)
+    
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
